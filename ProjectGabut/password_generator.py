337449b5e2846a90f9a7f6f62e9e7bc676a30199
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2023-02-19 14:57:16 +0700

    Password Generator

---
+++ ProjectGabut/password_generator.py
@@ -0,0 +1,20 @@
+import random
+subjek = ["1","2","3","4","5","6","7","8","9",
+'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+
+def generatePassword(obj):
+    randPassword = ""
+    randLength = random.randint(16,32)
+    
+    for i in range(0, randLength):
+        j = random.randint(0,60)
+        # print(obj[j])
+        randPassword += obj[j]
+        i+=1
+        
+    return randPassword
+
+if __name__ == '__main__':
+    print(generatePassword(subjek))
