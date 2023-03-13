889da01426d92988db55921ea493738ef11f9a79
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2023-03-13 19:32:14 +0700

    JWT Token Authorization

--- belajarflask/app/routes.py
+++ belajarflask/app/routes.py
@@ -1,13 +1,22 @@
-from app import app
+from app import app, response
 from app.controller import DosenController, UserController
 from flask import request
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 
 @app.route('/')
 def index():
     return 'Hello Flask App'
 
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return response.success(current_user, "Sukses")
+
 @app.route('/dosen', methods=['GET', 'POST'])
+@jwt_required()
 def dosens():
     if request.method == 'GET':
         return DosenController.index()
